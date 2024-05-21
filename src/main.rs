64c8d3f35464e6168077d008470ceab0ee54f6a9
webghast <victor-martinho@hotmail.com>
2024-05-20 21:05:18 -0300

    primeiro input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use 
 fn main() {
-    println!("Hello, world!");
+    let mut userinput = String::new();
+    let mut userinput2 = String::new();
+
+    println!("Por favor me diga seu nome");
+
+    io::stdin()
+        .read_line(&mut userinput)
+        .expect("Falha ao anotar seu nome!");
+
+    println!("Agora me diga sua idade!");
+
+    io::stdin()
+        .read_line(&mut userinput2)
+        .expect("Falaha ao anotar sua idade!");
+
+    println!("Pelos meus registros seu nome consta como : {userinput}");
+    println!("e sua idade como {userinput2}");
 }
